a4da9ccfeaebb1ae9a6c41810bac6693670f4f45
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-08-29 15:14:32 -0500

    Added CalculateAgeModel

---
+++ FirstResponsiveWebAppMiles/Models/CalculateAgeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstResponsiveWebAppMiles.Models
+{
+    public class CalculateAgeModel
+    {
+        public int Year { get; set; }
+        public String? Age { get; set; }
+        public DateTime CurrentDate { get; set; }
+
+        public String CalculateAge()
+        {
+            CurrentDate = DateTime.Now;
+            DateTime dateTime = CurrentDate.AddYears(-Year);
+            Age = dateTime.ToString("yyyy");
+            return Age;
+        }
+    }
+}
