c3aae29d87f52310fb5418448c353510945fe42f
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-08-29 14:20:35 -0500

    Added HomeController

---
+++ FirstResponsiveWebAppMiles/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppMiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+    }
+}
